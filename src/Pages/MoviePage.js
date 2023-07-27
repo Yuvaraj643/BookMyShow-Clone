a9e949e4a715409fb94e1ad2fe65f4e1b086fe4b
Yuvaraj643 <vyuvarajnani2001@gmail.com>
2023-07-27 12:34:56 +0530

    now

--- src/Pages/MoviePage.js
+++ src/Pages/MoviePage.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import CastSlider from "../components/Cast/CastSlider";
 import PosterSlider from "../components/PosterSlider/PosterSlider";
+import Loader from "../components/Loader/Loader";
 
 const Movie = () => {
   const { id } = useParams();
@@ -12,9 +13,19 @@ const Movie = () => {
   const [crew, setCrew] = useState([]);
   const [upcommingMovies, setUpcommingMovies] = useState([]);
 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   useEffect(() => {
     const requestUpcommingMovies = async () => {
-      const getUpcommingMovies = await axios.get(`/movie/${id}/recommendations`);
+      const getUpcommingMovies = await axios.get(
+        `/movie/${id}/recommendations`
+      );
       setUpcommingMovies(getUpcommingMovies.data.results);
     };
     requestUpcommingMovies();
@@ -57,48 +68,56 @@ const Movie = () => {
   }
   return (
     <>
-      <MovieHero />
-      <div className="my-3 container px-4 lg:w-1/2 lg:ml-64">
-        <div className="flex flex-col items-start gap-2">
-          <h2 className="text-gray-800 font-bold text-2xl">About the Movie</h2>
-          <p>{movie.overview}</p>
-        </div>
-        <div className="my-8">
-          <hr />
-        </div>
-        <div className="flex flex-col items-start">
-          <h1 className="text-gray-800 font-bold text-2xl">
-            Applicable Offers
-          </h1>
-          <div className="flex items-start gap-2 bg-yellow-100 border-yellow-400 border-dashed border-2 rounded-md px-4 py-3 w-96 my-5">
-            <img
-              src="https://in.bmscdn.com/offers/tnclogo/filmy-pass---rs-75-off-on-movies-filmypass99.jpg?26082021190202"
-              className="w-6 h-6"
-              alt=""
-            />
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <MovieHero />
+          <div className="my-3 container px-4 lg:w-1/2 lg:ml-64">
+            <div className="flex flex-col items-start gap-2">
+              <h2 className="text-gray-800 font-bold text-2xl">
+                About the Movie
+              </h2>
+              <p>{movie.overview}</p>
+            </div>
+            <div className="my-8">
+              <hr />
+            </div>
             <div className="flex flex-col items-start">
-              <h3 className="text-gray-900 text-lg">Filmy Pass</h3>
-              <p className="text-gray-600">10% off on Movie munchies!</p>
+              <h1 className="text-gray-800 font-bold text-2xl">
+                Applicable Offers
+              </h1>
+              <div className="flex items-start gap-2 bg-yellow-100 border-yellow-400 border-dashed border-2 rounded-md px-4 py-3 w-96 my-5">
+                <img
+                  src="https://in.bmscdn.com/offers/tnclogo/filmy-pass---rs-75-off-on-movies-filmypass99.jpg?26082021190202"
+                  className="w-6 h-6"
+                  alt=""
+                />
+                <div className="flex flex-col items-start">
+                  <h3 className="text-gray-900 text-lg">Filmy Pass</h3>
+                  <p className="text-gray-600">10% off on Movie munchies!</p>
+                </div>
+              </div>
+            </div>
+            <div className="w-full">
+              <CastSlider
+                title="Cast"
+                subtitle="Main cast"
+                cast={cast}
+                crew={crew}
+                isDark={false}
+              />
             </div>
           </div>
-        </div>
-        <div className="w-full">
-          <CastSlider
-            title="Cast"
-            subtitle="Main cast"
-            cast={cast}
-            crew={crew}
-            isDark={false}
-          />
-        </div>
-      </div>
-      <div className="gap-10 px-auto w-5/6 mx-auto">
-        <PosterSlider
-          images={upcommingMovies}
-          title="Recommended Movies"
-          isDark={false}
-        />
-      </div>
+          <div className="gap-10 px-auto w-5/6 mx-auto">
+            <PosterSlider
+              images={upcommingMovies}
+              title="Recommended Movies"
+              isDark={false}
+            />
+          </div>
+        </>
+      )}
     </>
   );
 };
