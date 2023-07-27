88d44c014c70bbe423f4a47892cd212743e183af
Yuvaraj643 <vyuvarajnani2001@gmail.com>
2023-07-27 12:30:28 +0530

    now

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React from 'react'
-import DefaultHOC from './HOC/DefaultHOC';
-import DefaultLayout from './layouts/DefaultLayout';
-import MovieHOC from './HOC/MovieHOC';
-import MovieLayout from './layouts/MovieLayout';
-import Plays from './Pages/Plays';
+import React, {useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import DefaultHOC from "./HOC/DefaultHOC";
+import DefaultLayout from "./layouts/DefaultLayout";
+import MovieHOC from "./HOC/MovieHOC";
+import MovieLayout from "./layouts/MovieLayout";
+import Plays from "./Pages/Plays";
 import axios from "axios";
+import Loader from "./components/Loader/Loader";
 //css
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 //axios
-axios.defaults.baseURL = 'https://api.themoviedb.org/3';
-axios.defaults.params ={};
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.params = {};
 axios.defaults.params["api_key"] = process.env.REACT_APP_API_KEY;
 
 
+
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+
   return (
     <>
-    <DefaultHOC path="/" exact element={<DefaultLayout/>} />
-    <MovieHOC path='/movie/:id' exact element={<MovieLayout/>} />
-    <DefaultHOC path="/plays" exact element={<Plays/>} />
+      {loading ? (
+        <Loader />
+      ) : (
+        <Router>
+          <Routes>
+            <Route path="/" exact element={<DefaultLayout />} />
+            <Route path="/movie/:id" exact element={<MovieLayout />} />
+            <Route path="/plays" exact element={<Plays />} />
+          </Routes>
+        </Router>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+const Loader = () => {
+  return (
+    <>
+    <section className="main"></section>
+      <div class="loader"></div>
+    </>
+  );
+};
+
+export default Loader;
